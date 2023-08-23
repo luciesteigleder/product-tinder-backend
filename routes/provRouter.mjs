4cedbc74db954127f2cfb34172cc563e60b36a2c
Lucie <luciesteigleder@gmail.com>
2023-08-23 15:48:41 +0200

    shop and prov routes are done

--- routes/provRouter.mjs
+++ routes/provRouter.mjs
@@ -1,6 +1,24 @@
 import express from "express";
 const router = express.Router();
 
-export default router.get("/", (req, res) => {
-    res.send("provRouter ok :)")
-})
+//get prov info
+router.get("/", (req, res) => {
+  res.send("provRouter ok !");
+});
+
+//Create a new prov profile
+router.post("/", (req, res) => {
+  res.send("provRouter post ok");
+});
+
+//Modify a new prov profile
+router.put("/", (req, res) => {
+  res.send("put route ok");
+});
+
+//get results from a search
+router.get("/search", (req, res) => {
+  res.send("search route ok");
+});
+
+export default router;
--- routes/shopRouter.mjs
+++ routes/shopRouter.mjs
@@ -1,6 +1,19 @@
 import express from "express";
 const router = express.Router();
 
-export default router.get("/", (req, res) => {
-    res.send("shopRouter ok :)")
-})
+//get shop info
+router.get("/", (req, res) => {
+  res.send("shopRouter ok !");
+});
+
+//Create a new shop profile
+router.post("/", (req, res) => {
+  res.send("shopRouter post ok");
+});
+
+//Modify a new shop profile
+router.put("/", (req, res) => {
+  res.send("put route ok");
+});
+
+export default router;
