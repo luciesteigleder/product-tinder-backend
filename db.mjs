79a0a3f5368cf8c37153af3b41dd071fd753bf86
Lucie <luciesteigleder@gmail.com>
2023-08-23 13:59:54 +0200

    middelwares to publish data on db are ok

--- db.mjs
+++ db.mjs
@@ -1,14 +1,17 @@
-import mongoose from "mongoose"
-import dotenv from "dotenv"
-dotenv.config()
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
 
-const client = mongoose.connect(process.env.DB_CONNECT, {
-useNewUrlParser: true,
-useUnifiedTopology: true
-}).then(() => {
-console.log('MongoDB Atlas connected!');
-}).catch(err => {
-console.log(err);
-});
+const client = mongoose
+  .connect(process.env.DB_CONNECT, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("MongoDB Atlas connected!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 export default client;
--- index.mjs
+++ index.mjs
@@ -1,23 +1,24 @@
 import express from "express";
 import mongoose from "mongoose";
-import catRouter from "./routes/catRouter.mjs"
+import catRouter from "./routes/catRouter.mjs";
 import db from "./db.mjs";
-import { categorySchema } from "./sample_data.mjs" 
+import {
+  User,
+  Shop,
+  Prov,
+  Category,
+  Tag,
+  Result,
+  Conversation,
+} from "./sample_data.mjs";
 const app = express();
 const PORT = 3050;
 
-app.use("/static", express.static("public"))
-app.use("/categories", catRouter)
-
+app.use("/static", express.static("public"));
+app.use("/categories", catRouter);
 
 // TESTING DB push to collection
-const Category = mongoose.model('test',categorySchema)
-app.post("/test", (req,res) => {
-  const test = new Category({ category_name: 'test category'})
-   test.save()
-   res.status(200).send('database entry succesful')
-})
-
+app.post("/test", (req, res) => {});
 
 app.listen(PORT, () => {
   console.log(`server running at http://localhost:${PORT}`);
