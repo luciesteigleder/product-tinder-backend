d7cfd39f9efd88bd64393989acb9215db1e18cb1
Antoine Struelens <antoine.struelens@gmail.com>
2023-08-24 09:35:52 +0200

    finish preparing all routes

--- routes/favRouter.mjs
+++ routes/favRouter.mjs
@@ -1,6 +1,19 @@
 import express from "express";
 const router = express.Router();
 
-export default router.get("/", (req, res) => {
+//Get favorites
+router.get("/", (req, res) => {
     res.send("favRouter ok :)")
 })
+
+//Add favorites
+router.post("/new", (req, res) => {
+    res.send("post request ok :)")
+})
+
+//Delete favorites
+router.delete("/", (req, res) => {
+    res.send("delete request ok :)")
+})
+
+export default router
--- routes/priceRouter.mjs
+++ routes/priceRouter.mjs
@@ -1,6 +1,24 @@
 import express from "express";
 const router = express.Router();
 
-export default router.get("/", (req, res) => {
+//Get price score
+router.get("/", (req, res) => {
     res.send("priceRouter ok :)")
 })
+
+//Add price score
+router.post("/new", (req, res) => {
+    res.send("post request ok :)")
+})
+
+//Update price score
+router.put("/", (req, res) => {
+    res.send("update request ok :)")
+})
+
+//Delete price score
+router.delete("/", (req, res) => {
+    res.send("delete request ok :)")
+})
+
+export default router
