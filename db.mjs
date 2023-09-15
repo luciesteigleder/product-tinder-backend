5455d568b3b9596562c7c9683050ce6a2212cf7e
Lucie <luciesteigleder@gmail.com>
2023-09-15 13:37:18 +0200

    fixed the aws c onstant error

--- db.mjs
+++ db.mjs
@@ -1,21 +1,7 @@
 import mongoose from "mongoose";
-//import aws from "aws-sdk";
 import dotenv from "dotenv";
 
 dotenv.config();
-// aws.config.update({
-//   accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-//   secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
-// });
-
-// const s3 = new aws.S3({
-//   region: process.env.AWS_REGION,
-//   connectKey: process.env.DB_CONNECT,
-// });
-
-// Access the connectKey value
-// const connectKey = s3.config.connectKey;
-
 const connectKey = process.env.DB_CONNECT;
 
 const client = mongoose
